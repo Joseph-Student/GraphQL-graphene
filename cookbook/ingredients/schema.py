0ac809c47f30b0bfb5ea97b4a18be19e03b822ac
Joseph Perez <24216045+Joseph-Student@users.noreply.github.com>
2018-12-14 13:11:41 -0400

    Correigo error y terminado.

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -27,7 +27,7 @@ class IngredientNode(DjangoObjectType):
 
 
 class CategoryFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr=['icontains'])
+    name = django_filters.CharFilter(lookup_expr='iexact')
 
     class Meta:
         model = Category
